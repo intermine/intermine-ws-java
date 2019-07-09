7b8608bd4df361f6baadbfed4702dbeee03f3b79
julie-sullivan <julie@flymine.org>
2019-07-09 11:38:02 +0100

    checkstyle

--- config/travis/run.sh
+++ config/travis/run.sh
@@ -8,20 +8,19 @@ GIT_GET="git clone --single-branch --depth 1"
 
 export GRADLE_OPTS='-server'
 
-if [ "$TEST_SUITE" = "intermine" ]; then
-    echo "RUNNING unit tests"
+echo "RUNNING unit tests"
 
-    echo "#---> Getting code from github"
-    $GIT_GET https://github.com/intermine/intermine-ws-java.git client
+echo "#---> Getting code from github"
+$GIT_GET https://github.com/intermine/intermine-ws-java.git client
 
-    echo "#---> Installing"
-    cd client && ./gradlew install
-    echo "#---> Running tests"
-    ./gradlew test
+echo "#---> Installing"
+cd client && ./gradlew install
+echo "#---> Running tests"
+./gradlew test
 
-    echo CHECKING results
-    ./config/lib/parse_test_report.py 'intermine'
+echo CHECKING results
+../config/lib/parse_test_report.py 'intermine'
 
-    echo ALL TESTS PASSED
+echo ALL TESTS PASSED
 
-fi
+
