6f08fb38ed5a03c4b31ad5dd1c42ba9b7ffa6260
julie-sullivan <julie@flymine.org>
2019-07-10 16:40:12 +0100

    travis!

--- config/travis/run.sh
+++ config/travis/run.sh
@@ -2,16 +2,39 @@
 
 set -e
 
-echo "RUNNING UNIT TESTS"
-
 GIT_GET="git clone --single-branch --depth 1"
 
-export GRADLE_OPTS='-server'
+export GRADLE_OPTS='-server -Dorg.gradle.daemon=false'
 
-echo "RUNNING unit tests"
+####################
+# set up test mine #
+####################
 
-echo "#---> Getting code from github"
-$GIT_GET https://github.com/intermine/intermine-ws-java.git client
+echo "#---> Getting code from github (for testmine)"
+${GIT_GET} https://github.com/intermine/intermine.git
+cd intermine
+
+echo "#---> Setting mine properties"
+source config/create-ci-properties-files.sh
+
+echo '#---> Installing python requirements'
+pip install -r config/lib/requirements.txt
+
+echo "#---> Set up database for testing"
+(cd intermine && ./gradlew createUnitTestDatabases)
+
+echo '#---> Building and releasing web application to test against'
+cd testmine && ./setup.sh
+
+sleep 60 # let webapp startup
+
+#############
+# Run tests #
+#############
+
+echo "#---> Getting code from github (client)"
+cd
+${GIT_GET} https://github.com/intermine/intermine-ws-java.git client
 
 echo "#---> Installing"
 cd client && ./gradlew install
