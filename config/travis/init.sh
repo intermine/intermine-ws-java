34ec9494bf1df15c5b27d295d9c57de3af4840c8
julie-sullivan <julie@flymine.org>
2019-07-10 15:49:06 +0100

    travis!

--- config/travis/init.sh
+++ config/travis/init.sh
@@ -8,15 +8,15 @@ BUILD_LOG=${HOME}/build.log
 
 export PSQL_USER=postgres
 
+echo "#---> Getting code from github (for testmine)"
+$GIT_GET https://github.com/intermine/intermine.git
+cd intermine
+
 echo "#---> Setting mine properties"
 source config/create-ci-properties-files.sh
 
 echo '#---> Installing python requirements'
 pip install -r config/lib/requirements.txt
-
-echo "#---> Getting code from github"
-$GIT_GET https://github.com/intermine/intermine.git
-cd intermine
 
 echo "#---> Set up database for testing"
 (cd intermine && ./gradlew createUnitTestDatabases)
--- config/travis/run.sh
+++ config/travis/run.sh
@@ -8,18 +8,19 @@ GIT_GET="git clone --single-branch --depth 1"
 
 export GRADLE_OPTS='-server'
 
+echo "RUNNING unit tests"
+
 echo "#---> Getting code from github"
 $GIT_GET https://github.com/intermine/intermine-ws-java.git client
 
 echo "#---> Installing"
 cd client && ./gradlew install
-
 echo "#---> Running tests"
 ./gradlew test
 
-echo "#---> Checking results"
+echo "#---> Checking test results"
 ../config/lib/parse_test_report.py 'intermine'
 
-echo "#---> ALL TESTS PASSED"
+echo "#---> All tests passed"
 
 
