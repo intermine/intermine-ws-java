20429fd608ee3ca9b0fbc1a2fcaf31d4a365153d
julie-sullivan <julie@flymine.org>
2019-07-10 15:21:52 +0100

    travis!

--- config/travis/init.sh
+++ config/travis/init.sh
@@ -2,48 +2,24 @@
 
 set -e
 
-if [ -z $(which wget) ]; then
-    # use curl
-    GET='curl'
-else
-    GET='wget -O -'
-fi
-
 GIT_GET="git clone --single-branch --depth 1"
 
 BUILD_LOG=${HOME}/build.log
 
 export PSQL_USER=postgres
-export KEYSTORE=${PWD}/keystore.jks
 
-echo "#---> Running $TEST_SUITE tests"
+echo "#---> Setting mine properties"
+source config/create-ci-properties-files.sh
 
-if [ "$TEST_SUITE" = "checkstyle" ]; then
-    exit 0 # nothing to do
-else
-    # Set up properties
-    source config/create-ci-properties-files.sh
+echo '#---> Installing python requirements'
+pip install -r config/lib/requirements.txt
 
-    echo '#---> Installing python requirements'
-    # Install lib requirements
-    pip install -r config/lib/requirements.txt
+echo "#---> Getting code from github"
+$GIT_GET https://github.com/intermine/intermine.git
+cd intermine
 
-    echo "#---> Getting code from github"
-    $GIT_GET https://github.com/intermine/intermine.git
-    cd intermine
+echo "#---> Set up database for testing"
+(cd intermine && ./gradlew createUnitTestDatabases)
 
-    # set up database for testing
-    (cd intermine && ./gradlew createUnitTestDatabases)
-
-    # We will need a fully operational web-application
-    echo '#---> Building and releasing web application to test against'
-    (cd testmine && ./setup.sh)
-
-    sleep 60 # let webapp startup
-
-    # Warm up the keyword search by requesting results, but ignoring the results
-    #$GET "$TESTMODEL_URL/service/search" > /dev/null
-    # Start any list upgrades
-    #$GET "$TESTMODEL_URL/service/lists?token=test-user-token" > /dev/null
-
-fi
+echo '#---> Building and releasing web application to test against'
+cd testmine && ./setup.sh
--- config/travis/run.sh
+++ config/travis/run.sh
@@ -2,25 +2,24 @@
 
 set -e
 
-echo "RUNNING test suite $TEST_SUITE"
+echo "RUNNING UNIT TESTS"
 
 GIT_GET="git clone --single-branch --depth 1"
 
 export GRADLE_OPTS='-server'
-
-echo "RUNNING unit tests"
 
 echo "#---> Getting code from github"
 $GIT_GET https://github.com/intermine/intermine-ws-java.git client
 
 echo "#---> Installing"
 cd client && ./gradlew install
+
 echo "#---> Running tests"
 ./gradlew test
 
-echo CHECKING results
+echo "#---> Checking results"
 ../config/lib/parse_test_report.py 'intermine'
 
-echo ALL TESTS PASSED
+echo "#---> ALL TESTS PASSED"
 
 
